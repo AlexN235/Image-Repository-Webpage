e1fa4a9ddefe26d1359501c639efe7fd7028e4b5
Alex Nguyen <alex.nguyen235@gmail.com>
2025-08-25 20:06:53 -0700

    create/maintain aws bucket and add images to that bucket

--- app.js
+++ app.js
@@ -3,9 +3,88 @@ const fs = require('fs');
 const path = require('path');
 const express = require('express');
 const fileUpload = require('express-fileupload');
+const { 
+    S3Client,
+    PutObjectCommand,
+    CreateBucketCommand,
+    ListBucketsCommand,
+    S3ServiceException,
+    } = require("@aws-sdk/client-s3");
 
 const app = express();
 const PORT = 8080;
+const client = new S3Client( {region: 'us-west-2'} );
+
+/*
+const input = { // ListBucketsRequest
+  MaxBuckets: 10,
+
+  Prefix: "STRING_VALUE",
+  BucketRegion: "us-west-2",
+};
+const command = new ListBucketsCommand(input);
+const response = client.send(command);
+
+*/
+let bucketName = "mys3bucket-project-imagereposite";
+
+async function createS3Bucket(bucketName) {
+    const input = {
+        Bucket: bucketName,
+        CreateBucketConfiguration: {
+            LocationConstraint: "us-west-2"
+        }
+    };
+    try {
+        const command = new CreateBucketCommand(input);
+        const response = client.send(command);
+        console.log('Bucket ${bucketName} created sucessfully: ', response);
+        return response;
+    }
+    catch (error) {
+        console.error(error);
+        if (error.name == "BucketAlreadyOwnedByYou") {
+            console.warn('Bucket ${bucketName} already exist')
+        }
+        else {
+            throw error;
+        }
+    }
+}
+
+createS3Bucket(bucketName)
+    .then(() => console.log("bucket creation attempt"))
+    .catch((err) => console.error("unhandled error in bucket creation", err));
+
+async function addImageToBucket(bucketName, key) {
+    const command = new PutObjectCommand({
+        Bucket: bucketName,
+        Key: key,
+    });
+    
+    try {
+        const response = await client.send(command);
+        console.log(response);
+    }
+    catch (error) {
+        if ( error instanceof S3ServiceException && error.name == "EntityTooLarge") {
+            console.error("S3 error uploading ${bucketName}. Object too large.");
+        }
+        else if (error instanceof S3ServiceException) {
+            console.error("S3 error uploading ${bucketName}.");
+        }
+        else {
+            throw error;    
+        }
+        
+    }
+}
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
 
 app.use(fileUpload({
         limits: {
@@ -15,12 +94,6 @@ app.use(fileUpload({
     })
 );
 
-app.use(express.static('public'));
-
-app.get('/', (req, res) => {
-    res.send('Hello World');
-});
-
 app.post('/upload', (req,res) => {
     const { image } = req.files;
     
@@ -28,6 +101,9 @@ app.post('/upload', (req,res) => {
     
     // Check if image type.
     if(!/^image/.test(image.mimetype)) return res.sendStatus(400);
+    
+    // Add image to bucket
+    addImageToBucket(bucketName, image.name)
     
     image.mv(__dirname + '/upload/' + image.name);
         
@@ -38,16 +114,5 @@ app.listen(PORT, () => {
    console.log('Example app listening on port ${PORT}'); 
 });
 
-/*
-fs.readFile('main.html', function (err, html) {
-    if (err) throw err;
-    
-    http.createServer(function(req, res) {
-        res.writeHeader(200, {"Content-Type": "text/html"});
-        res.write(html);
-        res.end();
-    }).listen(PORT);
-});
-*/
 
 // http://localhost:8080 to run.
